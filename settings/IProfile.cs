01ea3b5b8e14c5c2e9263d1078bd3164b4a2f93d
Greg Meess <greg@sutromachine.com>
2019-12-04 12:10:41 -0800

    Remove extra layer of gsCore in directory structure

---
+++ settings/IProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace gs.interfaces
+{
+    public interface IProfile
+    {
+        string ManufacturerName { get; }
+        string ModelIdentifier { get; }
+        string ProfileName { get; }
+
+        double MachineBedSizeXMM { get; }
+        double MachineBedSizeYMM { get; }
+        double MachineBedSizeZM01M { get; }
+
+        IProfile Clone();
+    }
+}
