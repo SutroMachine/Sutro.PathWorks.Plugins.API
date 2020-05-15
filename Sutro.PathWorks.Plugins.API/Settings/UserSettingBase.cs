0acc4f1432769c778e29bca015d18e53b01af52c
Greg Meess <greg@sutromachine.com>
2020-05-15 09:43:33 -0700

    Move files to subproject, hit 0% debt with NDepend

---
+++ Sutro.PathWorks.Plugins.API/Settings/UserSettingBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Sutro.PathWorks.Plugins.API.Settings
+{
+    public abstract class UserSettingBase
+    {
+        private readonly Func<string> nameF;
+        private readonly Func<string> descriptionF;
+
+        public string Name => nameF();
+        public string Id { get; }
+        public string Description => descriptionF();
+
+        public UserSettingGroup Group { get; }
+
+        public abstract void LoadFromRaw(object settings);
+
+        public abstract void ApplyToRaw(object settings);
+
+        public abstract object GetFromRaw(object settings);
+
+        public abstract void SetToRaw(object settings, object value);
+
+        public abstract ValidationResult Validation { get; }
+
+        public abstract ValidationResult Validate(object value);
+
+        protected UserSettingBase(string id, Func<string> nameF, Func<string> descriptionF = null, UserSettingGroup group = null)
+        {
+            this.nameF = nameF;
+            this.descriptionF = descriptionF;
+            Group = group;
+            Id = id;
+        }
+    }
+}
---
+++ Sutro.PathWorks.Plugins.API/Settings/UserSettingBaseGeneric.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Sutro.PathWorks.Plugins.API.Settings
+{
+    public abstract class UserSettingBase<TSettings> : UserSettingBase
+    {
+        // Can be used to hide settings in inherited UserSettingsCollection classes
+        public bool Hidden { get; set; } = false;
+
+        protected UserSettingBase(string id,
+            Func<string> nameF,
+            Func<string> descriptionF = null,
+            UserSettingGroup group = null) : base(id, nameF, descriptionF, group)
+        {
+        }
+
+        public override void LoadFromRaw(object settings)
+        {
+            LoadFromRaw((TSettings)settings);
+        }
+
+        public override void ApplyToRaw(object settings)
+        {
+            ApplyToRaw((TSettings)settings);
+        }
+
+        public override object GetFromRaw(object settings)
+        {
+            return GetFromRaw((TSettings)settings);
+        }
+
+        public override void SetToRaw(object settings, object value)
+        {
+            SetToRaw((TSettings)settings, value);
+        }
+
+        public abstract void LoadFromRaw(TSettings settings);
+
+        public abstract void ApplyToRaw(TSettings settings);
+
+        public abstract object GetFromRaw(TSettings settings);
+
+        public abstract void SetToRaw(TSettings settings, object value);
+    }
+}
